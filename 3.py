49d4c760784f361cc2893d1a70eb1c2405b6a2c8
Mikhail Miroshnikov <mirrosha@gmail.com>
2022-10-12 01:36:03 +0300

    Chande 3.py

--- 3.py
+++ 3.py
@@ -1,8 +1,17 @@
 
-files_list = ['example/1.txt', 'example/2.txt', 'example/3.txt' ]
+files_list = ['example/1.txt', 'example/2.txt', 'example/3.txt']
 size_dict = {}
 for path in files_list:
-    size_dict[path] = len(open(path,"r", encoding="utf-8").readlines())
+    with open(path, 'r', encoding='utf-8') as f:
+        size_dict[path] = len(f.readlines())
 
-for i in size_dict:
-    
+with open('out.txt', 'w+', encoding='utf-8') as file_out:
+    for i in files_list:
+        path = min(size_dict, key=size_dict.get)
+        size = size_dict[path]
+        with open(path, 'r', encoding='utf-8') as file_read:
+            text = file_read.read()
+            file_out.write(path + '\n')
+            file_out.write(str(size) + '\n')
+            file_out.write(text + '\n')
+            size_dict.pop(path)
